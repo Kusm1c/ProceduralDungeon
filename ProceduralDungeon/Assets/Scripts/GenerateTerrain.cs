49f6723e3e12d6a59e9ba62fa0fd9e6f11a34bad
esdragor <baudouinlapeze9@gmail.com>
2023-11-14 18:51:59 +0100

    horizontalWall stretch

--- ProceduralDungeon/Assets/Scripts/GenerateTerrain.cs
+++ ProceduralDungeon/Assets/Scripts/GenerateTerrain.cs
@@ -7,36 +7,39 @@ using Random = UnityEngine.Random;
 
 public class GenerateTerrain : MonoBehaviour
 {
-   [Header("Terrain Parameters")]
-   [SerializeField] private Vector2Int terrainDimensions;
-   [SerializeField] private Transform terrainTransform;
-   [SerializeField] private Transform cameraLaveRT;
-   [SerializeField] private TileSO Wall;
-   [SerializeField] private List<TileSO> Layers = new ();
-   
-   [Header("Shader Parameters")]
-   [SerializeField] private float thicknessParam = 0.95f;
-   [SerializeField] private Color gridColor = Color.red ;
-   [SerializeField] private Color lineColor = Color.white ;
-   private string pathFloorShader = "ProceduralDungeon/Floor/SG_Floor";
-   private string pathTilingInShader = "_Tiling";
-   private string pathThicknessInShader = "_LineThickness";
-   private string pathGridColorInShader = "_GridColor";
-   private string pathLineColorInShader = "_LineGridColor";
-   
-   //Reférence Object
-   [HideInInspector]public GameObject terrainRef;
-   [HideInInspector]public GameObject cameraLavaRef;
-   
-   [Header("Random parameters")]
-   [SerializeField] [Range(1, 10000)] private int worldSeed = 1;
-   [SerializeField] private bool useRandomSeed = false;
-   
-   private float[,] mapData;
-   private List<Vector2Int> AvailablePositions = new();
-   private Random.State stateBeforeStep3;
-   
-   private Dictionary<int, TileSO> _dicTileSO = new ();
+    [Header("Terrain Parameters")] [SerializeField]
+    private Vector2Int terrainDimensions;
+
+    [SerializeField] private Transform terrainTransform;
+    [SerializeField] private Transform cameraLaveRT;
+    [SerializeField] private TileSO Wall;
+    [SerializeField] private List<TileSO> Layers = new();
+
+    [Header("Shader Parameters")] [SerializeField]
+    private float thicknessParam = 0.95f;
+
+    [SerializeField] private Color gridColor = Color.red;
+    [SerializeField] private Color lineColor = Color.white;
+    private string pathFloorShader = "ProceduralDungeon/Floor/SG_Floor";
+    private string pathTilingInShader = "_Tiling";
+    private string pathThicknessInShader = "_LineThickness";
+    private string pathGridColorInShader = "_GridColor";
+    private string pathLineColorInShader = "_LineGridColor";
+
+    //Reférence Object
+    [HideInInspector] public GameObject terrainRef;
+    [HideInInspector] public GameObject cameraLavaRef;
+
+    [Header("Random parameters")] [SerializeField] [Range(1, 10000)]
+    private int worldSeed = 1;
+
+    [SerializeField] private bool useRandomSeed = false;
+
+    private float[,] mapData;
+    private List<Vector2Int> AvailablePositions = new();
+    private Random.State stateBeforeStep3;
+
+    private Dictionary<int, TileSO> _dicTileSO = new();
 
     private void Start()
     {
@@ -72,7 +75,7 @@ public class GenerateTerrain : MonoBehaviour
             name = so.type.ToString()
         };
         goP.transform.parent = transform;
-        
+
         for (int i = 0; i < numToUse; i++)
         {
             int index = Random.Range(0, positions.Count);
@@ -80,28 +83,80 @@ public class GenerateTerrain : MonoBehaviour
             AvailablePositions.Remove(pos);
             mapData[pos.x, pos.y] = (int)so.type;
             positions.RemoveAt(index);
-            
+
             GenerateTile(so.tilePrefab, goP.transform, new Vector3(pos.x, 0.1f, pos.y), so.color2D);
         }
     }
 
     public void Generate3DWorld()
     {
+        float[,] mapDataSave = new float[terrainDimensions.x, terrainDimensions.y];
         for (int y = 0; y < terrainDimensions.y; y++)
         {
             for (int x = 0; x < terrainDimensions.x; x++)
             {
-                _dicTileSO.TryGetValue((int)mapData[x, y], out TileSO so);
+                mapDataSave[x, y] = mapData[x, y];
+            }
+        }
+
+        for (int y = 0; y < terrainDimensions.y; y++)
+        {
+            for (int x = 0; x < terrainDimensions.x; x++)
+            {
+                _dicTileSO.TryGetValue((int)mapDataSave[y, x], out TileSO so);
                 if (so == null) continue;
-                if (so.Model3D_S1.Count > 0)
+
+                byte XisGood = 0;
+                
+                int posTile = 0;
+                int indexTile = (int)mapDataSave[y, x];
+                if (indexTile == -1) continue;
+                while (posTile + x < terrainDimensions.x && indexTile == mapDataSave[y, x + posTile])
                 {
-                    int index = Random.Range(0, so.Model3D_S1.Count);
-                    GameObject go = Instantiate(so.Model3D_S1[index], transform);
-                    go.transform.position = new Vector3(x, go.transform.localScale.y/2f, y);
-                    if (x == 0 || x == terrainDimensions.x - 1)
-                        go.transform.Rotate(Vector3.up, 90);
+                    mapDataSave[y, x + posTile] = -1;
+
+                    posTile++;
                 }
 
+                if (posTile <= 1)
+                {
+                    XisGood = 1;
+                    posTile = 0;
+                    indexTile = (int)mapDataSave[y, x];
+                    if (indexTile != -1)
+                        while (posTile + y < terrainDimensions.y && indexTile == mapDataSave[y + posTile, x])
+                        {
+                            mapDataSave[y + posTile, x] = -1;
+                            posTile++;
+                        }
+                    if (posTile <= 1)
+                        XisGood = 2;
+                }
+
+                if (XisGood == 0 && so.TillingTextureModel && so.Model3D_S1.Count > 0)
+                {
+                    int index = Random.Range(0, so.Model3D_S1.Count);
+
+                    GameObject go = Instantiate(so.Model3D_S1[index], transform);
+                    
+                    Vector3 scale = go.transform.localScale;
+                    scale.x = (float)posTile;
+                    go.transform.localScale = scale;
+                    go.transform.position = new Vector3((x + posTile)/2f, scale.y/2f, y);
+                }
+                
+                
+                if (XisGood == 2 && so.Model3D_S1.Count > 0)
+                {
+                    int index = Random.Range(0, so.Model3D_S1.Count);
+
+                    GameObject go = Instantiate(so.Model3D_S1[index], transform);
+                    
+                    go.transform.position = new Vector3(x, transform.localScale.y/2f, y);
+
+                    if ((x == 0 && y != 0) || x == terrainDimensions.x - 1)
+                        go.transform.Rotate(Vector3.up, 90);
+                }
             }
         }
     }
@@ -112,29 +167,29 @@ public class GenerateTerrain : MonoBehaviour
         _dicTileSO.Clear();
         mapData = new float[terrainDimensions.x, terrainDimensions.y];
     }
-    
+
     public void GenerateData()
     {
         ResetData();
-        
+
         if (!useRandomSeed)
             Random.InitState(worldSeed);
         List<Vector2Int> unavailablePositions = new List<Vector2Int>();
         UtilsToolTerrain.InitData(ref mapData, ref AvailablePositions, terrainDimensions,
-            ref  unavailablePositions);
+            ref unavailablePositions);
 
         GameObject goP = new GameObject
         {
             name = Wall.type.ToString()
         };
         goP.transform.parent = transform;
-        
+
         _dicTileSO.Add((int)Wall.type, Wall);
 
         for (int i = 0; i < unavailablePositions.Count; i++)
         {
-           GenerateTile(Wall.tilePrefab, goP.transform, 
-               new Vector3(unavailablePositions[i].x, 0.1f, unavailablePositions[i].y), Wall.color2D);
+            GenerateTile(Wall.tilePrefab, goP.transform,
+                new Vector3(unavailablePositions[i].x, 0.1f, unavailablePositions[i].y), Wall.color2D);
         }
 
         List<Vector2Int> ValidPositions = new List<Vector2Int>();
@@ -148,10 +203,11 @@ public class GenerateTerrain : MonoBehaviour
                 if (UtilsTerrainData.CheckAllConditions(Layers, i, AvailablePositions[j], terrainDimensions, mapData))
                     ValidPositions.Add(AvailablePositions[j]);
             }
+
             ChoosePosToUse(Layers[i], ValidPositions);
         }
     }
-    
+
     [ContextMenu("Generate Terrain")]
     public void GenerateTerrainMesh()
     {
@@ -160,7 +216,7 @@ public class GenerateTerrain : MonoBehaviour
             DestroyImmediate(transform.GetChild(i).gameObject);
             i--;
         }
-        
+
         //Get Transform
         GameObject terrain = Instantiate(terrainTransform.gameObject, transform);
         terrain.name = "Terrain";
@@ -175,49 +231,51 @@ public class GenerateTerrain : MonoBehaviour
         mesh.triangles = UtilsToolTerrain.GenerateSimpleFloorTriangles();
         mesh.uv = UtilsToolTerrain.GenerateSimpleFloorUV(terrainDimensions);
         mesh.RecalculateNormals();
-       
+
         //RendererPart
         terrain.AddComponent<MeshRenderer>();
         MeshRenderer meshRenderer = terrain.GetComponent<MeshRenderer>();
         meshRenderer.sharedMaterial = new Material(Shader.Find(pathFloorShader));
         meshRenderer.sharedMaterial.name = "M_Terrain_01";
-        
+
         //Setup Camera
         SetupCameraLava();
 
         //Setup Shader 
         meshRenderer.sharedMaterial.SetFloat(pathThicknessInShader, thicknessParam);
-        meshRenderer.sharedMaterial.SetVector(pathTilingInShader, new Vector4(terrainDimensions.x, terrainDimensions.y, 0, 0));
+        meshRenderer.sharedMaterial.SetVector(pathTilingInShader,
+            new Vector4(terrainDimensions.x, terrainDimensions.y, 0, 0));
         meshRenderer.sharedMaterial.SetColor(pathGridColorInShader, gridColor);
         meshRenderer.sharedMaterial.SetColor(pathLineColorInShader, lineColor);
-       
+
         terrainRef = terrain;
-        
+
         //Setup terrain Collider
         terrain.AddComponent<MeshCollider>();
         MeshCollider meshCollider = terrain.GetComponent<MeshCollider>();
         meshCollider.sharedMesh = mesh;
-        
+
         //NavMesh
         terrain.AddComponent<NavMeshSurface>();
     }
-    
+
 
     [ContextMenu("Update Terrain Material")]
-   public void UpdateMaterial()
-   {
-       MeshRenderer meshRenderer = terrainRef.GetComponent<MeshRenderer>();
-       meshRenderer.sharedMaterial.SetFloat(pathThicknessInShader, thicknessParam);
-       meshRenderer.sharedMaterial.SetVector(pathTilingInShader, new Vector4(terrainDimensions.x, terrainDimensions.y, 0, 0));
-       meshRenderer.sharedMaterial.SetColor(pathGridColorInShader, gridColor);
-       meshRenderer.sharedMaterial.SetColor(pathLineColorInShader, lineColor);
-   }
+    public void UpdateMaterial()
+    {
+        MeshRenderer meshRenderer = terrainRef.GetComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial.SetFloat(pathThicknessInShader, thicknessParam);
+        meshRenderer.sharedMaterial.SetVector(pathTilingInShader,
+            new Vector4(terrainDimensions.x, terrainDimensions.y, 0, 0));
+        meshRenderer.sharedMaterial.SetColor(pathGridColorInShader, gridColor);
+        meshRenderer.sharedMaterial.SetColor(pathLineColorInShader, lineColor);
+    }
 
-   public void SetupCameraLava()
-   {
-       GameObject cameraLava = Instantiate(cameraLaveRT.gameObject, transform);
-       cameraLava.transform.position = new Vector3((float)terrainDimensions.x / 2, 5, (float)terrainDimensions.y / 2);
-       cameraLava.GetComponent<Camera>().orthographicSize = (float)(terrainDimensions.x + terrainDimensions.y) / 4;
-       cameraLavaRef = cameraLava;
-   }
+    public void SetupCameraLava()
+    {
+        GameObject cameraLava = Instantiate(cameraLaveRT.gameObject, transform);
+        cameraLava.transform.position = new Vector3((float)terrainDimensions.x / 2, 5, (float)terrainDimensions.y / 2);
+        cameraLava.GetComponent<Camera>().orthographicSize = (float)(terrainDimensions.x + terrainDimensions.y) / 4;
+        cameraLavaRef = cameraLava;
+    }
 }
