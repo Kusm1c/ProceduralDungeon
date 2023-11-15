7c84767753dc01bc682e233aa1aae024e3709134
Damien <damien.harle@gmail.com>
2023-11-15 11:48:57 +0100

    FIX: fuk le big ternary c un switch mtn

--- ProceduralDungeon/Assets/Scripts/UtilsTerrainData.cs
+++ ProceduralDungeon/Assets/Scripts/UtilsTerrainData.cs
@@ -44,19 +44,14 @@ public static class UtilsTerrainData
 
         if (posCond is { x: -1, y: -1 }) return false;
 
-        return (soCondition.possibility == Possibility.Must)
-            ? CheckMustOrNot(posCond, soCondition.type, mapData, true)
-            :
-            (soCondition.possibility == Possibility.MustNot)
-                ? CheckMustOrNot(posCond, soCondition.type, mapData, false)
-                :
-                (soCondition.possibility == Possibility.Can)
-                    ? CheckCanOrCant(posCond, soCondition.type, mapData , true)
-                    :
-                    (soCondition.possibility == Possibility.Cant)
-                        ? CheckCanOrCant(posCond, soCondition.type, mapData , false)
-                        :
-                        throw new System.Exception("Error in UtilsTerrainData.CheckCondition");
+        return soCondition.possibility switch
+        {
+            Possibility.Must => CheckMustOrNot(posCond, soCondition.type, mapData, true),
+            Possibility.MustNot => CheckMustOrNot(posCond, soCondition.type, mapData, false),
+            Possibility.Can => CheckCanOrCant(posCond, soCondition.type, mapData , true),
+            Possibility.Cant => CheckCanOrCant(posCond, soCondition.type, mapData , false),
+            _ => throw new System.Exception("Error in UtilsTerrainData.CheckCondition")
+        };
     }
 
     private static bool CheckCanOrCant(Vector2Int posCond, Type soConditionType, float[,] mapData, bool can)
@@ -64,8 +59,8 @@ public static class UtilsTerrainData
         bool cond = mapData[posCond.x, posCond.y] == (int)soConditionType;
         return can switch
         {
-            true when Random.Range(0, 100) < 25 => true,
-            false when Random.Range(0, 100) < 25 => false,
+            true when Random.Range(0, 100) < 85 => true,
+            false when Random.Range(0, 100) < 85 => false,
             _ => (can) ? cond : !cond
         };
     }
